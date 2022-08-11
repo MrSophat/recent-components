945569f75cff65e9a1aeec1928c5810943fc0375
pphatDev <leatsophat02@gmail.com>
2022-08-11 13:49:15 +0700

    update ui

--- resources/views/components/web/index.blade.php
+++ resources/views/components/web/index.blade.php
@@ -25,12 +25,40 @@
                 </div>
             </div>
 
+            <div class="grid grid-cols-4 gap-5 ">
+                <div class="col-span-1">
 
-            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-2">
+                    <div class="shadow-lg rounded-2xl relative bg-red-500 pt-14">
+
+                        <div class="absolute top-6 left-5 z-10">
+                            <img class="object-cover w-16 h-16 rounded-full outline outline-white" src="https://cdn.dribbble.com/userupload/2776604/file/original-6683b4718849db80771553e606fc52d8.png?compress=1&resize=1024x768" alt="">
+                        </div>
+
+                        <div class="bg-slate-400 rounded-2xl p-5 min-h-[12rem] relative">
+                            <h1 class="mt-5 font-semibold leading-7 text-lg">BitCoins</h1>
+                            <p class="leading-7 line-clamp-2 text-slate-600">The Bitcoin price is $23,944.60, a change of 3.68% over the past 24 hours as of 11:00 p.m. The recent price action in Bitcoin left the token's market</p>
+                        </div>
+
+                        {{-- <div class="bg-red-500 translate-y-14 rounded-xl">
+                            <div class="px-5">
+                                <img class="object-cover w-20 h-20 rounded-full -translate-y-10 outline outline-white" src="https://cdn.dribbble.com/userupload/2776604/file/original-6683b4718849db80771553e606fc52d8.png?compress=1&resize=1024x768" alt="">
+                                <h1>Title Product</h1>
+                            </div>
+                        </div> --}}
+
+
+
+                    </div>
+
+                </div>
+            </div>
+
+            {{-- <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-2">
+                
                 <div class="h-72 col-span-1 rounded-md last:col-span-2 first:col-span-3 p-5 bg-slate-800">
                     sss
                 </div>
-            </div>
+            </div> --}}
 
 
         </div>
--- resources/views/web.blade.php
+++ resources/views/web.blade.php
@@ -15,17 +15,7 @@
         @include('components.admin.layouts.navigation')
 
         <div class="max-w-[100rem] mx-auto">
-            <div class="sidebar">
-                <div class="sticky top-5 z-[51] -ml-0.5">
-                    @include('components.web.layouts.sidebar')
-                </div>
-            </div>
-            <div class="lg:ml-[15.5rem]">
-                <div class="p-5">
-                    @yield('content')
-                </div>
-                @include('components.web.layouts.credit')
-            </div>
+            @yield('content')
         </div>
 
         @include('components.web.layouts.footer')
